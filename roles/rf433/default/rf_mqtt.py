b39c517ef305196d1bf691d795fbd903b11d9071
Burkart <burkart.poneleit@gmail.com>
2020-08-02 23:08:02 +0200

    fix service and add water message

--- roles/rf433/default/rf_mqtt.py
+++ roles/rf433/default/rf_mqtt.py
@@ -37,14 +37,14 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, message):
     sys.stdout.write("Message received: "  + message.topic[23:] + '/' + message.payload + '\n')
     sys.stdout.flush()
-    print "Message received: "  + message.topic[23:] + '/' + message.payload 
+    print("Message received: "  + message.topic[23:] + '/' + message.payload)
     system, device = message.topic[23:].split('/')
     command = commands.get(message.payload)
     system = format(int(system), '005b')
     device = devices.get(device)
     sys.stdout.write('/home/pi/433Utils/RPi_utils/send ' + system + ' ' + device + ' ' + str(command) + '\n')
     sys.stdout.flush()
-    print '/home/pi/433Utils/RPi_utils/send ' + system + ' ' + device + ' ' + str(command) 
+    print('/home/pi/433Utils/RPi_utils/send ' + system + ' ' + device + ' ' + str(command))
     subprocess.call(['/home/pi/433Utils/RPi_utils/send', system, device, str(command)])
 
 def on_intertechno_message(client, userdata, message):
@@ -60,9 +60,22 @@ def on_intertechno_message(client, userdata, message):
     # print '/home/pi/433Utils/RPi_utils/codesend ' + str(code)
     subprocess.call(['/home/pi/433Utils/RPi_utils/codesend', str(code)])
 
+def on_water_message(client, userdata, message):
+    sys.stdout.write("\nWasser: " + message.payload)
+    sys.stdout.flush()
+    command = commands.get(message.payload)
+    sys.stdout.write("\nCommand: " + command)
+    if command == '1':
+        sys.stdout.write("\nWasser Marsch!")
+        subprocess.call(['/usr/local/misc/pin27-low.py'])
+    else:
+        sys.stdout.write("\nWasser aus")
+        subprocess.call(['/usr/local/misc/pin27-high.py'])
+    sys.stdout.flush()
+
 Connected = False   #global variable for the state of the connection
 
-broker_address= "ankh-morpork.fritz.box"  #Broker address
+broker_address= "10.244.0.5"  #Broker address
 port = 1883                         #Broker port
 # user = "yourUser"                    #Connection username
 # password = "yourPassword"            #Connection password
@@ -75,6 +88,7 @@ client.message_callback_add("/our place/commands/rf/a/#", on_intertechno_message
 client.message_callback_add("/our place/commands/rf/b/#", on_intertechno_message)
 client.message_callback_add("/our place/commands/rf/c/#", on_intertechno_message)
 client.message_callback_add("/our place/commands/rf/d/#", on_intertechno_message)
+client.message_callback_add("/our place/commands/water/garden/freshwater/#", on_water_message)
 
 
 client.connect(broker_address, port=port)          #connect to broker
@@ -84,13 +98,17 @@ client.loop_start()        #start the loop
 while Connected != True:    #Wait for connection
     time.sleep(0.1)
 
+sys.stdout.write("Connected")
+sys.stdout.flush()
+
 client.subscribe("/our place/commands/rf/#")
+client.subscribe("/our place/commands/water/garden/freshwater/#")
 
 try:
     while True:
         time.sleep(1)
 
 except KeyboardInterrupt:
-    print "exiting"
+    print("exiting")
     client.disconnect()
     client.loop_stop()
